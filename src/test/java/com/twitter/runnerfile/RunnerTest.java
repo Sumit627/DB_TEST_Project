9dca7717425ff713ad706de62ad86e48f0b715da
Sumit627 <45812843+Sumit627@users.noreply.github.com>
2021-04-19 12:43:13 +0530

    Update RunnerTest.java
    RunnerTest java file update to run all feature file

--- src/test/java/com/twitter/runnerfile/RunnerTest.java
+++ src/test/java/com/twitter/runnerfile/RunnerTest.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = {"src/test/resources/TwitterProfileSectionUpdate.feature"},
-//        features = {"src/test/resources/"},
+
+        features = {"src/test/resources/"},
         glue = {"com/twitter/stepdefs"},
         plugin = {"html:target/cucumber-html-report",
                 "json:target/cucumber.json"},
